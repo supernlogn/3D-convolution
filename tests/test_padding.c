5909e774a32cc1e364c2ca9cb6b9f0d43fda8718
supernlogn <ath.ioannis94@gmail.com>
2022-04-03 00:03:05 +0300

    This is a correction to a printing bug in print_mat. If the matrix was 2D then it would only print the first row. Now it prints all rows.

--- tests/test_padding.c
+++ tests/test_padding.c
@@ -3,40 +3,43 @@
 #include "private.h"
 
 // Test the zero padding
-void fill_mat(double * mat, const int length, const double val) {
-    for (int i = 0; i < length; ++i) {
-        mat[i] = val;
-    }
+void fill_mat(double *mat, const int length, const double val)
+{
+  for (int i = 0; i < length; ++i)
+  {
+    mat[i] = val;
+  }
 }
 
 void print_mat(const int rank, const double *mat, const int dims[])
 {
-    int * iterators = (int*) calloc(rank, sizeof(int));
-    int total_num_els = _dim_size(dims, rank);
-    for (int numels = 0; numels < total_num_els; numels += dims[rank-1])
+  int *iterators = (int *)calloc(rank, sizeof(int));
+  int total_num_els = _dim_size(dims, rank);
+  for (int numels = 0; numels < total_num_els; numels += dims[rank - 1])
+  {
+    const int lli = dims[rank - 1];
+    for (int i = 0; i < lli; ++i) // iterators[rank-1]
     {
-        const int lli = dims[rank-1];
-        for (int i = 0; i < lli; ++i) // iterators[rank-1]
-        {
-            printf("%0.3f ",mat[i]);
-            
-        }
-        printf("\n");
-        int keeper = 1;
-        for(int i = rank-2; i >= 0; --i)
-        {
-            iterators[i] += keeper;
-            if (iterators[i] < dims[i])
-            {
-              keeper = 0;
-            } else {
-              printf("\n");
-              keeper = 1;
-              iterators[i] = 0;
-            }
-        }
+      printf("%0.3f ", mat[numels + i]);
     }
-    free(iterators);
+    printf("\n");
+    int keeper = 1;
+    for (int i = rank - 2; i >= 0; --i)
+    {
+      iterators[i] += keeper;
+      if (iterators[i] < dims[i])
+      {
+        keeper = 0;
+      }
+      else
+      {
+        printf("\n");
+        keeper = 1;
+        iterators[i] = 0;
+      }
+    }
+  }
+  free(iterators);
 }
 
 void test1(const int add_to_N0, const int add_to_N1)
@@ -49,7 +52,7 @@ void test1(const int add_to_N0, const int add_to_N1)
   fill_mat(mat, N0 * N1, 1.0);
   int dim[] = {N0, N1};
   int res_dim[] = {N0 + add_to_N0, N1 + add_to_N1};
-  double * final_mat;
+  double *final_mat;
   _zero_pad_array(rank, mat, dim, res_dim, &final_mat);
   printf("dims = {%d,%d}\n", res_dim[0], res_dim[1]);
   print_mat(rank, final_mat, res_dim);
@@ -57,7 +60,7 @@ void test1(const int add_to_N0, const int add_to_N1)
 
 void main(int argc, char *argv[])
 {
-  test1(4,3);
+  test1(4, 3);
 
   return;
 }
